5de76d7f130a0a924cad0a4c422c12b1397ac875
Pgans <mhosp.gan@gmail.com>
2018-05-24 11:44:37 +0700

    procedure opd ipd opd_card

--- MySQL/servers/localhost/mbase_data/rf_referout_diag.sql
+++ MySQL/servers/localhost/mbase_data/rf_referout_diag.sql
@@ -1,32 +1,12 @@
-<<<<<<< HEAD
-
-SELECT DISTINCT
-rec_no
-refer_no
-icd10
-diagtype_id
-=======
-SET @vn = '0001907378';
-SELECT DISTINCT
-gcoffice.offid as hcode
-, '' rec_no
-,concat(gcoffice.offid, '-3-', a.refer_id ) refer_no
-,c.icd10_tm as  icd10
-,CASE
-WHEN b.dxt_id = 01 THEN '1'
-WHEN b.dxt_id = 02 THEN '5'
-WHEN b.dxt_id = 03 THEN '3'
-WHEN b.dxt_id = 04 THEN '4'
-WHEN b.dxt_id = 05 THEN '5'
-WHEN b.dxt_id = 06 THEN '6'
-WHEN b.dxt_id = 07 THEN '7'
-WHEN b.dxt_id = 08 THEN '8'
-WHEN b.dxt_id = 09 THEN '9'
-ELSE '5'
-END as diagtype_id
-FROM gcoffice , refers a
-INNER JOIN opd_diagnosis b ON a.visit_id = b.visit_id  AND b.is_cancel = 0
-INNER JOIN icd10new c ON b.icd10 = c.icd10  
+SET @vn = '0001907378'; 
+SELECT DISTINCT gcoffice.offid as hcode , '' rec_no ,concat(gcoffice.offid, '-3-', a.refer_id ) refer_no ,c.icd10_tm as icd10 ,
+CASE WHEN b.dxt_id = 01 THEN '1' 
+WHEN b.dxt_id = 02 THEN '5' 
+WHEN b.dxt_id = 03 THEN '3' WHEN b.dxt_id = 04 THEN '4' 
+WHEN b.dxt_id = 05 THEN '5' WHEN b.dxt_id = 06 THEN '6' 
+WHEN b.dxt_id = 07 THEN '7' WHEN b.dxt_id = 08 THEN '8' 
+WHEN b.dxt_id = 09 THEN '9' ELSE '5' END as diagtype_id FROM gcoffice , refers a 
+INNER JOIN opd_diagnosis b ON a.visit_id = b.visit_id AND b.is_cancel = 0 
+INNER JOIN icd10new c ON b.icd10 = c.icd10 
 WHERE a.visit_id = @vn
-AND a.is_cancel = 0
->>>>>>> a3c29ef6f75f25b67addb72f9c59d79dac383fc1
+AND a.is_cancel = 0 
--- MySQL/servers/localhost/mbase_data/สิทธิ์ว่าง - Copy.sql
+++
@@ -1,5 +0,0 @@
-SELECT a.CID , b.HN 
-FROM  population a, cid_hn b 
-WHERE  a.CID = b.CID 
-AND a.CID NOT IN (SELECT CID from deaths)
-AND a.INSCL = 00
